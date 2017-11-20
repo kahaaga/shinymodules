6cd1f7928dda4f1833926d56a11698c29a3786d1
kahaaga <kahaaga@gmail.com>
2017-11-20 14:17:26 +0100

    Added function to generate multiple checkboxUIs wrapper in tagList

--- R/checkboxUI.R
+++ R/checkboxUI.R
@@ -30,8 +30,67 @@ checkboxUI <- function(id,
     shiny::checkboxInput(
       inputId = ns("checkbox"), 
       label = label,
-      icon = icon,
       width = width
     )
   )
+}
+
+#' @title checkboxUIs
+#' @description Wrapper around shiny::checkboxInput
+#' @param ids String id for the checkboxex. If your code is modularised,
+#'     remember to wrap the id with the relevant session namespace function, e.g.
+#'     checkboxUIs(id = sapply(c("someinput", "anotherinput", session$ns))).
+#' @param labels Labels for the checkboxes. 
+#' @param widths The widths of the checkboxes. Must be valid css (i.e. 
+#'     "10\%" or "150px" will work; see shiny::validateCssUnit). To use the same 
+#'     width on all checkboxes, provide a single-text string or 
+#'     a vector with one text string. To use different widths for each checkbox,
+#'     provide a vector of custom css text strings with one element per 
+#'     action button.
+#' @param custom_css If provided, the  checkboxes are wrapped in divs each
+#'     styled by the custom_css input. For example, when creating multiple 
+#'     checkboxes one could provide custom_css = "display: inline-block;" to 
+#'     display them inline. To use the same style on all checkboxes, 
+#'     provide a single-text string or single-element vector. To use different
+#'     styles for each checkbox, provide a vector of custom css text strings
+#'     with one element per checkbox.
+#' @export
+checkboxUIs <- function(ids,
+                        labels = NULL,
+                        icons = NULL,
+                        widths = NULL,
+                        custom_css = NULL) {
+  
+  # Create a div 
+  ui.elements = list()
+  
+  for (i in 1:length(ids)) {
+    # Create a custom actionButton using the actionButtonUI wrapper that 
+    # ensures proper namespace handling.
+    
+    if (is.null(widths)) {
+      width = NULL
+    } else if (length(widths) == 1) {
+      width = widths
+    } else if (length(widths) > 1) {
+      width = widths[i]
+    }
+    
+    if (is.null(custom_css)) {
+      custom_css = NULL
+    } else if (length(custom_css) == 1) {
+      custom_css = custom_css
+    } else if (length(custom_css) > 1) {
+      custom_css = custom_css[i]
+    }
+    
+    ui.elements[[i]] = checkboxUI(
+      id = ids[i],
+      label = labels[i],
+      width = width,
+      custom_css = custom_css
+    )
+  }
+  
+  tagList(ui.elements)
 }
